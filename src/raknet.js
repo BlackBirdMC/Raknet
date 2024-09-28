f2242b22b6455d6012610db57d9a64f12d6b0319
KanadeBlue dw <Jaidenurity@gmail.com>
2024-09-28 11:13:34 +0000

    added frame

--- src/raknet.js
+++ src/raknet.js
@@ -1,11 +1,14 @@
 const dgram = require('dgram');
+const { EventEmitter } = require('events');
 const { createPacket, parsePacket, fragmentPacket } = require('./utils/packetUtils');
 const Serializer = require('./utils/serializer');
 const Compression = require('./utils/compression');
-const ReliabilityTool = require('./reliabilityTool');
+const ReliabilityTool = require('./utils/reliabilityTool');
+const Frame = require('./utils/frame');
 
-class RakNet {
+class RakNet extends EventEmitter {
     constructor(port) {
+        super();
         this.port = port;
         this.server = dgram.createSocket('udp4');
         this.clients = new Map();
@@ -18,7 +21,6 @@ class RakNet {
         });
 
         this.server.bind(this.port);
-        this.startKeepAlive();
     }
 
     onMessage(message, rinfo) {
@@ -32,17 +34,16 @@ class RakNet {
                 this.setConnectionTimeout(rinfo);
                 require('./packetHandlers/connectPacket')(payload, rinfo, this);
                 break;
-            case 0x01: 
-                require('./packetHandlers/disconnectPacket')(payload, rinfo, this);
+            case 0x01:
+                this.handleDisconnect(rinfo);
                 break;
-            case 0x02: 
+            case 0x02:
                 require('./packetHandlers/messagePacket')(payload, rinfo, this);
                 break;
             case 0x03:
-                this.reliabilityTool.acknowledge(payload.readUInt16BE(0)); 
                 require('./packetHandlers/acknowledgmentPacket')(payload, rinfo, this);
                 break;
-            case 0x04: 
+            case 0x04:
                 require('./packetHandlers/pingPacket')(payload, rinfo, this);
                 break;
             default:
@@ -50,14 +51,22 @@ class RakNet {
         }
     }
 
+    handleDisconnect(rinfo) {
+        console.log(`Client ${rinfo.address}:${rinfo.port} has disconnected.`);
+        this.emit("disconnect", rinfo);
+        this.clients.delete(`${rinfo.address}:${rinfo.port}`);
+    }
+
     sendPacket(packetId, data, address, port) {
         const serializedData = Serializer.serialize(data);
         const compressedData = Compression.compress(serializedData);
         const fragments = fragmentPacket(compressedData, 512); 
 
+        this.reliabilityTool.storePacketData(packetId, data);
+
         fragments.forEach((fragment, index) => {
             const packet = createPacket(packetId, Buffer.concat([Buffer.from([index]), fragment])); 
-            this.reliabilityTool.setTimeout(packetId); 
+            this.reliabilityTool.setTimeout(packetId);
             this.server.send(packet, port, address, (error) => {
                 if (error) {
                     console.error('Error sending packet:', error);
@@ -69,20 +78,20 @@ class RakNet {
     setConnectionTimeout(rinfo) {
         const timeout = setTimeout(() => {
             console.log(`Client ${rinfo.address}:${rinfo.port} has timed out.`);
-            this.clients.delete(rinfo.address + ':' + rinfo.port);
+            this.handleDisconnect(rinfo);
         }, 30000); 
 
-        this.clients.set(rinfo.address + ':' + rinfo.port, { rinfo, timeout });
+        this.clients.set(`${rinfo.address}:${rinfo.port}`, { rinfo, timeout });
     }
 
     startKeepAlive() {
         setInterval(() => {
             this.clients.forEach((client, key) => {
-                const pingPacket = createPacket(0x04, Buffer.from([])); 
+                const pingPacket = createPacket(0x04, Buffer.from([]));
                 this.server.send(pingPacket, client.rinfo.port, client.rinfo.address);
             });
         }, 10000); 
     }
 }
 
-module.exports = RakNet;
+module.exports = { RakNet, Frame, ReliabilityTool };
